d1dffa74e1b591d0bd02316b10b1395d6992c023
Muqthar Ahmed Quraishi J <muqthar401@gmail.com>
2022-07-27 09:26:33 +0530

    added aboutus component

--- pages/components/header/index.js
+++ pages/components/header/index.js
@@ -18,7 +18,7 @@ function Header() {
             </a>
 
             <a
-              href="#about"
+              href="/about"
               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
             >
               about
