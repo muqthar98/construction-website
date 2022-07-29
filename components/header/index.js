23e57c9c93f1d194c17754b0c6e25cef3b727ea1
Dharanish V <30527938+Dharanish3499@users.noreply.github.com>
2022-07-29 13:21:53 +0530

    projects route template done

--- components/header/index.js
+++ components/header/index.js
@@ -26,7 +26,7 @@ function Header() {
           </a>
 
           <a
-            href="#projects"
+            href="/projects"
             className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
           >
             projects
