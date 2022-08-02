91d90cee5de70f771bc5853a7417d300825e5fc7
muqthar98 <muqthar401@gmail.com>
2022-08-02 22:11:17 +0530

    changes in main index.js in

--- components/main/index.js
+++ components/main/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import About from "./About";
+import About from "./about";
 import Contact from "./contact";
 import Hero from "./hero";
 import Projects from "./projects";
