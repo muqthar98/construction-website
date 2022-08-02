eae33879d7cebe4e632aeaa2cf0b05ae945f0bc5
muqthar98 <muqthar401@gmail.com>
2022-08-02 21:39:02 +0530

    change a tag to link in header

--- components/header/index.js
+++ components/header/index.js
@@ -11,33 +11,33 @@ function Header() {
           </h1>
         </Link>
         <div className=" flex items-center justify-center p-6 text-gray-600 capitalize dark:text-gray-300">
-          <a
+          <Link
             href="/"
             className="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
           >
             home
-          </a>
+          </Link>
 
-          <a
+          <Link
             href="/about"
             className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
           >
             about
-          </a>
+          </Link>
 
-          <a
+          <Link
             href="/projects"
             className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
           >
             projects
-          </a>
+          </Link>
 
-          <a
+          <Link
             href="/contact"
             className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
           >
             contact
-          </a>
+          </Link>
         </div>
       </nav>
     </header>
