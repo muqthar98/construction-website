b49d3e7b99946106610822c0a3bb368fc4775118
Dharanish V <30527938+Dharanish3499@users.noreply.github.com>
2022-08-03 13:13:33 +0530

    about us container code change

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,26 +1,31 @@
 import React from "react";
-import image4 from "../../public/assets/image4.jpeg"
+import image4 from "../../public/assets/image4.jpeg";
 import Image from "next/image";
 
 function aboutUs() {
   return (
-    <>
-      <div className="min-h-screen place-items-center container grid md:grid-cols-2 grid-cols-1 pt-32 md:pt-0 gap-0">
+    <div className="min-h-screen bg-white dark:bg-gray-800 dark:text-white md:pt-0 place-items-center grid">
+      <div className="place-items-center grid container md:grid-cols-2 grid-cols-1 gap-0">
         <div>
-          <Image src={image4} alt="" width={300} height={300}/>
+          <Image src={image4} alt="" width={300} height={300} />
         </div>
         <div className="p-5">
-          <p>We Global Sanitary Consultancy was established on 14/01/2008 by Mr.Prabhu 
-          Jayapal and we are engaged in the business of construction, Renovation. Plumbing, Painting, 
-          Electrical, Carpentry. Modular kitchen,wardrobe, complete civil and sanitary works for 
-          residential and commercial building.We ensure that all our projects meet the necessary guidelines for the structural 
-          stability of the building and are subjected to many quality tests, starting from 
-          selection of the site, architects, consultants, vendors, material procurement 
-          followed by structural design to meet the quality guidelines set forth by the 
-          prevailing norms and codes of the country.</p>
+          <p>
+            We Global Sanitary Consultancy was established on 14/01/2008 by
+            Mr.Prabhu Jayapal and we are engaged in the business of
+            construction, Renovation. Plumbing, Painting, Electrical, Carpentry.
+            Modular kitchen,wardrobe, complete civil and sanitary works for
+            residential and commercial building.We ensure that all our projects
+            meet the necessary guidelines for the structural stability of the
+            building and are subjected to many quality tests, starting from
+            selection of the site, architects, consultants, vendors, material
+            procurement followed by structural design to meet the quality
+            guidelines set forth by the prevailing norms and codes of the
+            country.
+          </p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
