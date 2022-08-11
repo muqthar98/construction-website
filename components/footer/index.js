b225bb22aeb9a930e32be143401133de8a252d84
Dharanish V <30527938+Dharanish3499@users.noreply.github.com>
2022-08-11 16:27:08 +0530

    about us para and logo changes

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/assets/image32.jpg";
 
 function Footer() {
   return (
     <footer className="flex justify-center px-4 text-gray-800 bg-white dark:text-white dark:bg-gray-800">
-        <div className="container py-6">
-          {/* <h1 className="text-lg font-bold text-center lg:text-2xl">
+      <div className="container py-6">
+        {/* <h1 className="text-lg font-bold text-center lg:text-2xl">
             Never miss <br /> out any exclusive offers
           </h1> */}
 
-          {/* <div className="flex justify-center mt-6">
+        {/* <div className="flex justify-center mt-6">
             <div className="bg-white border rounded-md focus-within:ring dark:bg-gray-800 dark:border-gray-600 focus-within:border-blue-400 focus-within:ring-blue-300 focus-within:ring-opacity-40 dark:focus-within:border-blue-300">
               <div className="flex flex-wrap justify-between md:flex-row">
                 <input
@@ -24,50 +27,55 @@ function Footer() {
             </div>
           </div> */}
 
-          <hr className="h-px mt-6 border-gray-300 border-none dark:bg-gray-700" />
+        <hr className="h-px mt-6 border-gray-300 border-none dark:bg-gray-700" />
 
-          <div className="flex flex-col items-center justify-between mt-6 md:flex-row">
-            <div>
+        <div className="flex flex-col items-center justify-between mt-6 md:flex-row">
+          <div>
+            <a
+              href="#"
+              className="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300"
+            >
+              <Image
+                src={logo}
+                height={"50rem"}
+                width={"120rem"}
+                alt={"Global Developers"}
+              />
+            </a>
+          </div>
+
+          <div className="flex mt-4 md:m-0">
+            <div className="-mx-4">
               <a
                 href="#"
-                className="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300"
+                className="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline"
               >
-                Brand
+                Home
               </a>
-            </div>
-
-            <div className="flex mt-4 md:m-0">
-              <div className="-mx-4">
-                <a
-                  href="#"
-                  className="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline"
-                >
-                  Home
-                </a>
-                <a
-                  href="#"
-                  className="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline"
-                >
-                  About
-                </a>
-                <a
-                  href="#"
-                  className="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline"
-                >
-                  Projects
-                </a>
-                <a
-                  href="#"
-                  className="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline"
-                >
-                  Contact
-                </a>
-              </div>
+              <a
+                href="#"
+                className="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline"
+              >
+                About
+              </a>
+              <a
+                href="#"
+                className="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline"
+              >
+                Projects
+              </a>
+              <a
+                href="#"
+                className="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline"
+              >
+                Contact
+              </a>
             </div>
           </div>
         </div>
-      </footer>
-  )
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
